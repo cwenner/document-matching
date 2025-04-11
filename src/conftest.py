46cdf6e29f1f1ec03abc6024b68eea203c351467
Cenny Wenner <cwenner@gmail.com>
2025-04-11 19:01:37 +0200

    Black

--- src/conftest.py
+++ src/conftest.py
@@ -3,10 +3,16 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--skip-model", action="store_true", default=False, help="skip tests requiring a model"
+        "--skip-model",
+        action="store_true",
+        default=False,
+        help="skip tests requiring a model",
     )
     parser.addoption(
-        "--only-model", action="store_true", default=False, help="only run tests requiring a model"
+        "--only-model",
+        action="store_true",
+        default=False,
+        help="only run tests requiring a model",
     )
 
 
@@ -16,13 +22,17 @@ def pytest_configure(config):
 
 def pytest_collection_modifyitems(config, items):
     if config.getoption("--skip-model"):
-        skip_model = pytest.mark.skip(reason="skipping model tests with --skip-model option")
+        skip_model = pytest.mark.skip(
+            reason="skipping model tests with --skip-model option"
+        )
         for item in items:
             if "model" in item.keywords:
                 item.add_marker(skip_model)
 
     if config.getoption("--only-model"):
-        skip_non_model = pytest.mark.skip(reason="skipping non-model tests with --only-model option")
+        skip_non_model = pytest.mark.skip(
+            reason="skipping non-model tests with --only-model option"
+        )
         for item in items:
             if "model" not in item.keywords:
                 item.add_marker(skip_non_model)
