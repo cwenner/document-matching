65af1b83a5c1ded51111619b77d9a2cbcc5013ce
Cenny Wenner <cwenner@gmail.com>
2025-04-11 18:45:33 +0200

    Use kebab case for cmd arg

--- conftest.py
+++ conftest.py
@@ -3,10 +3,10 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--skipmodel", action="store_true", default=False, help="skip tests requiring a model"
+        "--skip-model", action="store_true", default=False, help="skip tests requiring a model"
     )
     parser.addoption(
-        "--onlymodel", action="store_true", default=False, help="only run tests requiring a model"
+        "--only-model", action="store_true", default=False, help="only run tests requiring a model"
     )
 
 
@@ -15,13 +15,14 @@ def pytest_configure(config):
 
 
 def pytest_collection_modifyitems(config, items):
-    if config.getoption("--skipmodel"):
-        skip_model = pytest.mark.skip(reason="skipping model tests with --skipmodel option")
+    if config.getoption("--skip-model"):
+        skip_model = pytest.mark.skip(reason="skipping model tests with --skip-model option")
         for item in items:
             if "model" in item.keywords:
                 item.add_marker(skip_model)
-    if config.getoption("--onlymodel"):
-        skip_non_model = pytest.mark.skip(reason="skipping non-model tests with --onlymodel option")
+
+    if config.getoption("--only-model"):
+        skip_non_model = pytest.mark.skip(reason="skipping non-model tests with --only-model option")
         for item in items:
             if "model" not in item.keywords:
                 item.add_marker(skip_non_model)
